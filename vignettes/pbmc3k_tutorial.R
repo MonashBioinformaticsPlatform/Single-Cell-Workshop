18989dc32c6594c38bf8d02df08ef2da2a6aa520
Adele Barugahare <aabarug@users.noreply.github.com>
2021-12-14 09:22:13 +1100

    commented out scale.data functions that were eval=FALSE in rmarkdown

--- vignettes/pbmc3k_tutorial.R
+++ vignettes/pbmc3k_tutorial.R
@@ -197,14 +197,15 @@ pbmc <- ScaleData(pbmc, features = all.genes)
 
 # Scaling is an essential step in the Seurat workflow, but only on genes that will be used as input to PCA. Therefore, the default in `ScaleData()` is only to perform scaling on the previously identified variable features (2,000 by default). To do this, omit the `features` argument in the previous function call, i.e.
 
-pbmc <- ScaleData(pbmc)
+# pbmc <- ScaleData(pbmc)
 
 # Your PCA and clustering results will be unaffected. However, Seurat heatmaps (produced as shown below with `DoHeatmap()`) require genes in the heatmap to be scaled, to make sure highly-expressed genes don't dominate the heatmap. To make sure we don't leave any genes out of the heatmap later, we are scaling all genes in this tutorial. 
 
 # **How can I remove unwanted sources of variation, as in Seurat v2?**
 # 
 # In `Seurat v2` we also use the `ScaleData()` function to remove unwanted sources of variation from a single-cell dataset. For example, we could 'regress out' heterogeneity associated with (for example) cell cycle stage, or mitochondrial contamination. These features are still supported in `ScaleData()` in `Seurat v3`, i.e.: 
-pbmc <- ScaleData(pbmc, vars.to.regress = 'percent.mt')
+
+#pbmc <- ScaleData(pbmc, vars.to.regress = 'percent.mt')
 
 # However, particularly for advanced users who would like to use this functionality, we strongly recommend the use of our new normalization workflow, `SCTransform()`. The method is described in our [paper](https://genomebiology.biomedcentral.com/articles/10.1186/s13059-019-1874-1), with a separate vignette using Seurat v3 [here](sctransform_vignette.html). As with `ScaleData()`, the function `SCTransform()` also includes a `vars.to.regress` parameter.
 
